39f0b8b5ba15a3a788bfebfc5e722849dbfad05a
Amiruddin Irsyad <amiruddinid@gmail.com>
2023-11-08 10:00:55 +0700

    add clarity and fix responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			screens:{
+				'pcb': { 'raw': '(max-height: 650px) and (min-width: 1200px)' },
+			},
 			keyframes: {
 				wiggle: {
 				  '0%, 100%': { transform: 'rotate(-4deg)' },
